58e4c391bd513db5affd130d899f3e27cc06c6af
Natali LaGuta <n.v.laguta2023@gmail.com>
2025-03-21 22:34:59 +0100

    fix App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,16 @@ function App() {
   };
 
   return (
-    <>
-      <div className="card">
-        {/* Пример использования ShopItemFunc */}
+    <div className="container">
+      <div className="background-element">
+      </div>
+      <div className="highlight-window">
+        <div className='highlight-overlay'></div>
+      </div>
+      <div className="window">
         <ShopItemFunc item={item} />
       </div>
-    </>
+    </div>
   );
 }
 
